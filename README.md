e4b2ea4fda38d86a36a9f615544824061ed836ea
Isaac Frank <codingwithisaac@gmail.com>
2023-05-29 12:24:38 +0100

    PySelGram 2.0 1. Ignore useless files 2. Add a requirements.txt 3. Update README to explain the changes 4. Use ffmpeg and selenium-wire to get video and audio and combine both

--- README.md
+++ README.md
@@ -2,22 +2,98 @@
 
 # Watch Video Tutorial [HERE](https://youtu.be/4FzYyARA_ak)
 
+## UPDATE
+
+Due to Instagram restrictions, one must be logged in to be able to view videos and images, which is why you need to specify `-u` and `-p` flags
+
+Due to Instagram hashing, masking and cloaking algorithm, getting the download link of a video is extremely tedius, which is why I had to get all XHR request (using selenium-wire) and reverse engineer those requests which we can use to download the audio and video separately, and combined these using ffpmeg
+
+## REQUIREMENTS
+
+All requirements for python are in the `requirements.txt` file
+
+### FFMPEG INSTALLATION
+
+<details>
+<summary>How To Install FFMPEG</summary>
+
+Confirm that you have installed ffmpeg by running `ffmpeg` and it isn't installed, do this
+
+#### Windows:
+
+1. Visit the official FFmpeg website at https://ffmpeg.org/download.html.
+2. Scroll down to the "Windows" section and click on the link corresponding to "Windows Builds" to access the download page.
+3. On the download page, you will find multiple options. Choose the one that matches your system architecture (32-bit or 64-bit).
+4. Download the static build of FFmpeg by clicking on the "Download Build" link.
+5. Extract the downloaded ZIP file to a location on your computer.
+6. Add the FFmpeg binary path to your system's PATH environment variable. This step allows you to run FFmpeg from the command line without specifying the full path. Here's how you can add it:
+
+   - Open the Start menu and search for "Environment Variables."
+   - Click on "Edit the system environment variables."
+   - In the System Properties window, click on the "Environment Variables" button.
+   - In the "System variables" section, scroll down and select the "Path" variable.
+   - Click on the "Edit" button.
+   - Click on the "New" button and add the path to the folder where you extracted the FFmpeg binaries (e.g., C:\ffmpeg\bin).
+   - Click "OK" to save the changes.
+
+7. Open a new command prompt window and type ffmpeg to verify that FFmpeg is correctly installed. You should see the FFmpeg command-line tool information printed in the console.
+
+#### macOS:
+
+1. The easiest way to install FFmpeg on macOS is by using Homebrew. Open the Terminal application.
+2. If you don't have Homebrew installed, run the following command to install it:
+
+   ```bash
+   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+   ```
+
+3. Once Homebrew is installed, you can install FFmpeg by running the following command:
+
+   ```bash
+   brew install ffmpeg
+   ```
+
+4. Wait for the installation to complete. Homebrew will download and install the FFmpeg package along with its dependencies.
+5. After the installation finishes, you can verify that FFmpeg is installed by typing ffmpeg in the Terminal. The FFmpeg command-line tool information should be displayed.
+
+#### Linux (Ubuntu/Debian):
+
+1. Open a terminal window.
+2. Run the following command to install FFmpeg:
+
+   ```bash
+   sudo apt-get update
+   sudo apt-get install ffmpeg
+   ```
+
+3. Wait for the installation to complete. The package manager will download and install FFmpeg along with its dependencies.
+4. After the installation finishes, you can verify that FFmpeg is installed by typing ffmpeg in the terminal. The FFmpeg command-line tool information should be displayed.
+</details>
+
 ### All Arguments
+
 `-v` or `--video` = Specify the link is a video link e.g `python main.py -v`
 
 `-i` or `--image` = Specify the link is an image link e.g `python main.py -i`
 
 `-l` or `--link` = Link to image/video e.g `python main.py -i -l https://www.instagram.com/p/CEYfEKHDTo6/`
 
-`-f` or `--filename` = Name of the file with extention e.g `python main.py -i -l https://www.instagram.com/p/CEYfEKHDTo6/ -f downloaded.jpg`
+`-u` or `--username` = Add account username e.g `python main.py -i -u username https://www.instagram.com/p/CEYfEKHDTo6/`
 
+`-p` or `--password` = Add account password e.g `python main.py -i -u username -p password https://www.instagram.com/p/CEYfEKHDTo6/`
+
+`-f` or `--filename` = Name of the file with extention e.g `python main.py -i -l https://www.instagram.com/p/CEYfEKHDTo6/ -f downloaded.jpg`
 
 ## How to use
 
-1. Download chromedriver from https://chromedriver.chromium.org/downloads
+1. MAKE SURE you have ffmpeg installed, [read this manual above](#ffmpeg-installation)
 
-2. Copy the `chromediver.exe` to `./chromedriver` path
+2. Download chromedriver from https://chromedriver.chromium.org/downloads
 
-3. Check argument variables and 
+3. Copy the `chromediver.exe` to `./chromedriver` path
 
-4. Run `python main.py [options] -f filename.[ext]`
+4. Install the requirements by running `pip install -r requirements.txt` or `python -m pip install -r requirements.txt`
+
+5. Check argument variables and
+
+6. Run `python main.py [options] -u username -p password -f filename.[ext]`
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from ntpath import join
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import requests
-import shutil
 import argparse
 import os
+import re
+import shutil
+import subprocess
+from glob import glob
+from time import sleep, time
+
+import requests
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from seleniumwire import webdriver
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--video", help="Specify the link is a video link",
@@ -17,28 +22,66 @@ parser.add_argument("-l", "--link", type=str,
                     help="Link to image/video")
 parser.add_argument("-f", "--filename", type=str,
                     help="Name of the file with extention")
+parser.add_argument("-u", "--username", type=str,
+                    help="Username to log in as")
+parser.add_argument("-p", "--password", type=str,
+                    help="Password of the user to log in as")
 args = parser.parse_args()
 
-dir_name = os.path.dirname(os.path.realpath(__file__))
-c_driver = os.path.join(dir_name, 'chromedriver\chromedriver')
 
+dir_name = os.path.dirname(os.path.realpath(__file__))
+c_driver = os.path.join(dir_name, 'chromedriver', 'chromedriver')
+c_service = Service(executable_path=c_driver)
 
 # Change this value to point to your downloaded chromedriver
 CHROME_DRIVER_PATH = c_driver
+OUTPUT_EXT = ".done.mp4"
+
+# Make sure ffmpeg is installed
+try:
+    subprocess.check_output(f"which ffmpeg", shell=True)
+except subprocess.CalledProcessError:
+    raise Exception("FFMPEG is not installed!")
 
 options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-driver = webdriver.Chrome(
-    executable_path=CHROME_DRIVER_PATH, options=options)
 
-By.CLASS_NAME
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(service=c_service, options=options)
+
+
+def login(username, password):
+    driver.get("https://instagram.com")
+    user_data_name = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+    user_data_password = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+    user_data_name.clear()
+    user_data_password.clear()
+    user_data_name.send_keys(username)
+    user_data_password.send_keys(password)
+
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, "button[type='submit']"))).click()
+    sleep(10)  # Sleep for 10 seconds
+
+
 def downloadImage(url, filename):
     driver.get(url)
     element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "FFVAD"))
+        EC.presence_of_element_located(
+            (By.XPATH, '//*[contains(@id, "mount_0_0_")]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div[1]/div/div/div/div/div/div/div[1]/img'))
     )
     s = element.get_attribute('src')
     r = requests.get(s, stream=True)
+
+    print(s, filename)
+    if (filename == None):
+        filename = f"{time()}.png"
+    print(s, filename)
 
     with open(filename, 'wb') as f:
         shutil.copyfileobj(r.raw, f)
@@ -47,18 +90,109 @@ def downloadImage(url, filename):
 def downloadVideo(url, filename):
     driver.get(url)
     element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "tWeCl"))
+        EC.presence_of_element_located((By.TAG_NAME, "video"))
     )
     s = element.get_attribute('src')
-    r = requests.get(s, stream=True)
+    if (filename == None):
+        filename = f"{time()}"
 
-    with open(filename, 'wb') as f:
+    driver.wait_for_request("^.*\.mp4.*bytestart.*$", 20)
+
+    includedFiles = []
+    mediaFiles = []
+
+    for request in driver.requests:
+        if request.response and request.response.status_code == 200 and re.search("^.*\.mp4.*bytestart.*$", request.url) != None:
+            key = re.search("\d/\d*.*\.mp4", request.url)
+            if key:
+                if (key.group(0) not in includedFiles):
+                    mediaFiles.append(request.url.split("bytestart")[0])
+                    includedFiles.append(key.group(0))
+
+    r = requests.get(mediaFiles[0], stream=True)
+    with open(filename+".mp4", 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024*1024):
             if chunk:
                 f.write(chunk)
 
+    for index in [-1, -2]:
+        r = requests.get(mediaFiles[index], stream=True)
+        with open(f"{filename}{index}.mp3", 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024*1024):
+                if chunk:
+                    f.write(chunk)
 
-video, image, filename, link = args.video, args.image, args.filename, args.link
+    combineAudioWithVideo(filename)
+
+
+def cleanUp(filename):
+    directory = os.path.dirname(os.path.realpath(__file__))
+    pattern = os.path.join(directory, filename)
+    file_paths = glob(f"{pattern}*")
+    print("Deleting redundant files...")
+    for file_path in file_paths:
+        if (not file_path.endswith(OUTPUT_EXT)):
+            os.remove(file_path)
+
+
+def getAudioAndVideoFiles(filename):
+    initialVideo = f"{filename}.mp4"
+    firstAudio = f"{filename}-1.mp3"
+    secondAudio = f"{filename}-2.mp3"
+
+    size1 = os.path.getsize(firstAudio)
+    size2 = os.path.getsize(secondAudio)
+    size3 = os.path.getsize(initialVideo)
+
+    potentialHDVideoFile = ""
+    audioFile = ""
+    videoFile = ""
+
+    if size1 < size2:
+        audioFile = firstAudio
+        potentialHDVideoFile = secondAudio
+    elif size1 > size2:
+        audioFile = secondAudio
+        potentialHDVideoFile = firstAudio
+    else:
+        audioFile = firstAudio
+        potentialHDVideoFile = secondAudio
+
+    hdSize = os.path.getsize(potentialHDVideoFile)
+
+    if hdSize < size3:
+        videoFile = initialVideo
+    elif hdSize > size2:
+        videoFile = potentialHDVideoFile
+    else:
+        videoFile = initialVideo
+
+    return (videoFile, audioFile)
+
+
+def combineAudioWithVideo(filename):
+    video, audio = getAudioAndVideoFiles(filename)
+
+    print("Audio file ", audio)
+    print("Video file ", video)
+
+    fullFile = f"{filename}{OUTPUT_EXT}"
+
+    subprocess.run(['ffmpeg', '-i', video, '-i', audio,
+                   '-c', 'copy', f"{fullFile}"])
+    cleanUp(filename)
+
+    for i in range(4):
+        print()
+
+    print("Video download complete")
+    print("Video saved to", fullFile)
+
+
+video, image, filename, link, username, password = args.video, args.image, args.filename, args.link, args.username, args.password
+
+login(username, password)
+
 
 if video:
     downloadVideo(link, filename)
